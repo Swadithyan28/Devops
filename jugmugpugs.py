a60d5a5c85201f5a9409ef02304d2f82d29f77a1
Swadithyan B <114463025+Swadithyan28@users.noreply.github.com>
2024-03-01 10:20:50 +0530

    Create jugmugpugs.py
    i created jugmugpug

---
+++ jugmugpugs.py
@@ -0,0 +1,26 @@
+number = int(input("Enter the number :"))
+res=""
+if  number %3 ==0:
+  res+="jugs"
+if  number %5 ==0:
+  res+="mugs"
+if number %7 ==0:
+  res+="pugs"
+
+
+2.
+
+number = int(input("Enter the number :"))
+string = str(number)
+result =[]
+res=""
+if  number %3 ==0:
+  result.append("jugs")
+if  number %5 ==0:
+  result.append("mugs")
+if number %7 ==0:
+  result.append("pugs")
+
+for i in range(len(result)):
+  res += result.pop()
+print(res)
